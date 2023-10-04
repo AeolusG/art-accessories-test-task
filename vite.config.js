6f2ac9d43acf7e20acf2efcdfdd7231d102a0564
Olga <olga68ko@gmail.com>
2023-10-04 20:41:19 +0300

    fix the bug

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: '/art-accessories-test-task/',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
